cf3afeea19dc9297d6df494850cb8dd03328c7cc
Hernan <hernan1994gonzalez@gmail.com>
2025-06-23 01:20:40 -0300

    carrito simple

--- app.js
+++ app.js
@@ -84,50 +84,77 @@ const aparecerImagen = (imagen) => {
 //Logica carrito 
 const favheart = document.querySelectorAll(".fav")
 
+const valorTotal = document.querySelector(".total")
+let total = 0;
 
 favheart.forEach(boton => {
 boton.addEventListener("click",(e) => {
 
   const tarjeta = e.target.closest('.card-productos');
+
   const descripcion = tarjeta.querySelector('.card-descripcion').textContent;
-  const precio = tarjeta.querySelector('.precio').textContent;
+  const precioTexto = tarjeta.querySelector('.precio').textContent;
+  const foto = tarjeta.querySelector(".img-producto").src;
+ const precioaNumero = parseInt(precioTexto.replace(/\D/g, ""));
+
+
+
 
 
   
   console.log('Descripción:', descripcion);
-  console.log('Precio:', precio);
-
-
+  console.log('Precio:', precioTexto);
 
 let carrito = document.querySelector(".icono-carrito")
 
 const elements=document.createElement("li")
+elements.setAttribute("data-precio", precioaNumero);
 elements.classList.add("agregados")
-
 
 elements.innerHTML = `
 
     <div class ="precio-descripcion">
-
+        <i class="fa-solid fa-xmark eliminar" data-precio="${precioaNumero}"></i>
+        <img src = ${foto} class "foto-producto">
         <p>${descripcion}</p>
-        <p>${precio}</p>
+        <p class="precio-producto">${precioTexto}</p>
+         
         
 
     </div>
 `
 
 carrito.appendChild(elements)
+
+total += precioaNumero;
+valorTotal.textContent=`$${total.toLocaleString("es-AR")}`;
+
+
+
+elements.querySelector(".eliminar").addEventListener("click", () => {
+  const precioProducto = parseInt(elements.getAttribute("data-precio"));
+  total -= precioProducto;
+  valorTotal.textContent = `$${total.toLocaleString("es-AR")}`;
+  elements.remove();
+});
+
+
+
 })
+
 
 });
 
+
+
+/*
 const iconCarrito = document.querySelector(".icono-carrito")
 const agregados= document.querySelector(".agregados")
 iconCarrito.addEventListener ("click",() => {
  
 
 
-})
+})*/
 
 
 
